24e4ae53d2089eb5e3645bb038f66e9ed9a2d7bf
joshschaerer <joshua.schaerer@students.fhnw.ch>
2022-06-06 02:58:11 +0200

    removed nav icon for security

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import { chartOne } from './visualizations/chart-1.js';
-import { chartTwo } from './visualizations/chart-2.js';
-import { chartFour } from './visualizations/chart-4.js';
-import { chartFive } from './visualizations/chart-5.js';
+import { chartOne } from "./visualizations/chart-1.js";
+import { chartTwo } from "./visualizations/chart-2.js";
+import { chartFour } from "./visualizations/chart-4.js";
+import { chartFive } from "./visualizations/chart-5.js";
 
 chartOne();
 chartTwo();
@@ -10,50 +10,50 @@ chartFive();
 
 /*=============== SHOW HIDDEN MENU ===============*/
 const showMenu = (toggleId, navbarId) => {
-	const toggle = document.getElementById(toggleId),
-		navbar = document.getElementById(navbarId);
+  const toggle = document.getElementById(toggleId),
+    navbar = document.getElementById(navbarId);
 
-	if (toggle && navbar) {
-		toggle.addEventListener('click', () => {
-			/* Show menu */
-			navbar.classList.toggle('show-menu');
-			/* Rotate toggle icon */
-			toggle.classList.toggle('rotate-icon');
-		});
-	}
+  if (toggle && navbar) {
+    toggle.addEventListener("click", () => {
+      /* Show menu */
+      navbar.classList.toggle("show-menu");
+      /* Rotate toggle icon */
+      toggle.classList.toggle("rotate-icon");
+    });
+  }
 };
-showMenu('nav-toggle', 'nav');
+showMenu("nav-toggle", "nav");
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll('section[id]');
+const sections = document.querySelectorAll("section[id]");
 
 function scrollActive() {
-	const scrollY = window.pageYOffset;
+  const scrollY = window.pageYOffset;
 
-	sections.forEach((current) => {
-		const sectionHeight = current.offsetHeight,
-			sectionTop = current.offsetTop - 58,
-			sectionId = current.getAttribute('id');
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id");
 
-		if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-			document
-				.querySelector('.nav__menu a[href*=' + sectionId + ']')
-				.classList.add('link--active');
-		} else {
-			document
-				.querySelector('.nav__menu a[href*=' + sectionId + ']')
-				.classList.remove('link--active');
-		}
-	});
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("link--active");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("link--active");
+    }
+  });
 }
-window.addEventListener('scroll', scrollActive);
+window.addEventListener("scroll", scrollActive);
 
 /*=============== DARK LIGHT THEME ===============*/
-const themeButton = document.getElementById('nav-theme');
-const darkTheme = 'dark-theme';
+const themeButton = document.getElementById("nav-theme");
+const darkTheme = "dark-theme";
 
 // Activate / deactivate the theme manually with the button
-themeButton.addEventListener('click', () => {
-	// Add or remove the dark / icon theme
-	document.body.classList.toggle(darkTheme);
+themeButton.addEventListener("click", () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
 });
